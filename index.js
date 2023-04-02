89e23f6c7a00f69ecf0918f768cf8794c0ae1036
Hari <raicubogdan811@gmail.com>
2023-04-02 04:45:57 +0300

    major1

--- index.js
+++ index.js
@@ -1,106 +1,105 @@
 // JavaScript Document
 
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(bodyParser.json({ extended: true }));
 
+app.use("/files", express.static("files"));
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
-app.get("/style.css", (req, res) => {
-    res.sendFile(process.cwd() + "/files/css/style.css");
+app.get("/presentation", function (req, res) {
+  res.sendFile(path.join(__dirname, "/presentation.html"));
 });
-app.get("/structure.css", (req, res) => {
-    res.sendFile(process.cwd() + "/files/css/structure.css");
-});
-app.get("/tts.js", (req, res) => {
-    res.sendFile(process.cwd() + "/files/js/tts.js");
-});
-
-
-
-
 
 // sendFile will go here
-app.post('/poem', function(req, res) {
-	// BUILD POEM PROMT
-	let promt = 'write a short poem with ' + req.body.strophes + ' strophes, each having ' + req.body.verses + ' verses about ' + req.body.theme + ', containing the follwing words: ' + req.body.keywords;
-	
-	
-	
-	// BUILD API REQUESTS OPTIONS
-	const options = {
-  		method: 'POST',
-  		headers: {
-			accept: 'application/json',
-			'content-type': 'application/json',
-			'X-API-KEY': 'b8b8ef51-2052-4a6c-a023-a2ae60805ab7'
-		},
-		body: JSON.stringify({
-			enable_google_results: 'true',
-			enable_memory: false,
-			input_text: promt
-		})
-	};
-	
-	const image_options = {
-		method: 'GET',
-		headers: {
-			accept: 'application/json'
-		}
-	};
-	const image_url = 'https://api.unsplash.com/search/photos?query=' + req.body.theme + '&count=1&client_id=kcdrQBRXObctoic4LCCsyMdEBSjuMjEk8sEWHwrnVQc';
+app.post("/poem", function (req, res) {
+  // BUILD POEM PROMT
+  let promt =
+    "write a short poem with " +
+    req.body.strophes +
+    " strophes, each having " +
+    req.body.verses +
+    " verses about " +
+    req.body.theme +
+    ", containing the follwing words: " +
+    req.body.keywords;
 
-	
-	async function get_poem() {
-		const response = await fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=en', options);
-		
-		let poem_json = await response.json();
-		let poem = poem_json.message;
+  // BUILD API REQUESTS OPTIONS
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "X-API-KEY": "b8b8ef51-2052-4a6c-a023-a2ae60805ab7",
+    },
+    body: JSON.stringify({
+      enable_google_results: "true",
+      enable_memory: false,
+      input_text: promt,
+    }),
+  };
 
-		return poem;
-	}
-	async function get_image() {
-		const image_response = await fetch(image_url, image_options);
-		
-		let image_poem_json = await image_response.json();
-		let link = image_poem_json.results[0].urls.small;
-		
-		return link;
-	}
-	
-	
-	// CALL API'S
-	async function get_data() {
-		try {
-			// CALL CHATSONIC API
-			let poem = await get_poem();
-			// CALL UNSPLASH API
-			let link = await get_image();
-			
-			// BUILD RESPONSE
-			var obj = new Object();
-			obj.poem = poem;
-			obj.link = link;
-			
-			res.end(JSON.stringify(obj));
-			res.sendStatus(200);
-			
-		} catch (error) {
-			poem = 'Sorry, we could not generate a poem for you at this time, please try again later';
-			link = 'https://www.freeiconspng.com/uploads/error-icon-7.png';
-		}
-	}
+  const image_options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  };
+  const image_url =
+    "https://api.unsplash.com/search/photos?query=" +
+    req.body.theme +
+    "&count=1&client_id=kcdrQBRXObctoic4LCCsyMdEBSjuMjEk8sEWHwrnVQc";
 
-	get_data();
-	
+  async function get_poem() {
+    const response = await fetch(
+      "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=en",
+      options
+    );
+
+    let poem_json = await response.json();
+    let poem = poem_json.message;
+
+    return poem;
+  }
+  async function get_image() {
+    const image_response = await fetch(image_url, image_options);
+
+    let image_poem_json = await image_response.json();
+    let link = image_poem_json.results[0].urls.small;
+
+    return link;
+  }
+
+  // CALL API'S
+  async function get_data() {
+    try {
+      // CALL CHATSONIC API
+      let poem = await get_poem();
+      // CALL UNSPLASH API
+      let link = await get_image();
+
+      // BUILD RESPONSE
+      var obj = new Object();
+      obj.poem = poem;
+      obj.link = link;
+
+      res.end(JSON.stringify(obj));
+      res.sendStatus(200);
+    } catch (error) {
+      poem =
+        "Sorry, we could not generate a poem for you at this time, please try again later";
+      link = "https://www.freeiconspng.com/uploads/error-icon-7.png";
+    }
+  }
+
+  get_data();
 });
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log("Server started at http://localhost:" + port);
